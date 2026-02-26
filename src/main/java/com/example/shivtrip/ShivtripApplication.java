b873b3bfb897c082e154414d588271aebe6bdd24
Shivraj Akuskar <shivrajakuskar7@gmail.com>
2026-02-26 17:46:24 +0530

    setup

---
+++ src/main/java/com/example/shivtrip/ShivtripApplication.java
@@ -0,0 +1,13 @@
+package com.example.shivtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShivtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShivtripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shivtrip/ShivtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shivtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShivtripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
